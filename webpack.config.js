5912d4bb2c3dcb262299c7bd23ec3fa71e08eafb
Amita-Roy <sardana.amita2@gmail.com>
2021-02-19 15:52:56 +0100

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ module.exports = {
     contentBase: './dist',
   },
   module: {
-    loaders: [{ exclude: ['node_modules'], loader: 'babel', test: /\.js?$/ }],
     rules: [
       {
         test: /\.css$/i,
@@ -14,20 +13,6 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
-    ],
-  },
-
-  resolve: {
-    alias: {
-      config$: './configs/app-config.js',
-      react: './vendor/react-master',
-    },
-    extensions: ['', 'js', 'jsx'],
-    modules: [
-      'node_modules',
-      'bower_components',
-      'shared',
-      '/shared/vendor/modules',
     ],
   },
 };
