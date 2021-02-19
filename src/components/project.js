9a03dc186ec192ce1c3d3694615d9629c2033c46
Amita-Roy <sardana.amita2@gmail.com>
2021-02-19 15:30:59 +0100

    Update lint errors in test files

--- src/components/project.js
+++ src/components/project.js
@@ -1,38 +1,38 @@
 import Todo from './todo';
 
 class Project {
-    constructor(name) {
-        this.name = name;
-        this.todos = [];
-    }
+  constructor(name) {
+    this.name = name;
+    this.todos = [];
+  }
 
-    static fromJSON(json) {
-        const project = new Project(json.name);
-        project.todos = json.todos.map(
-            (todo) => new Todo(todo.title, todo.description, todo.dueDate, todo.priority),
-        );
-        return project;
-    }
+  static fromJSON(json) {
+    const project = new Project(json.name);
+    project.todos = json.todos.map(
+      (todo) => new Todo(todo.title, todo.description, todo.dueDate, todo.priority),
+    );
+    return project;
+  }
 
-    getName() {
-        return this.name;
-    }
+  getName() {
+    return this.name;
+  }
 
-    getTodos() {
-        return this.todos;
-    }
+  getTodos() {
+    return this.todos;
+  }
 
-    addTodo(title, description, dueDate, priority) {
-        this.todos.push(new Todo(title, description, dueDate, priority));
-    }
+  addTodo(title, description, dueDate, priority) {
+    this.todos.push(new Todo(title, description, dueDate, priority));
+  }
 
-    removeTodoAt(index) {
-        this.todos.splice(index, 1);
-    }
+  removeTodoAt(index) {
+    this.todos.splice(index, 1);
+  }
 
-    updateTodo(todo, index) {
-        this.todos[index] = todo;
-    }
+  updateTodo(todo, index) {
+    this.todos[index] = todo;
+  }
 }
 
 export default Project;
